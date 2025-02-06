8a66f80020119b0300e1c2ff5cc4119113b7ec75
glowfi <86939591+glowfi@users.noreply.github.com>
2025-02-06 21:11:08 +0530

    fix merge_multi_dataset

--- utils/merge_multi_dataset.py
+++ utils/merge_multi_dataset.py
@@ -36,23 +36,27 @@ if not multi_dataset_location:
     print("no dataser folder location entered")
     sys.exit(1)
 
+files = []
 for directory in glob.glob(f"{multi_dataset_location}/*"):
-    for file_name in file_names:
-        final = []
-        for file in glob.glob(f"{directory}/*"):
-            if file.lower().find(file_name.lower()) != -1:
-                with open(file) as fp:
-                    data = json.load(fp)
-                    if file_name == "subreddits.json":
-                        for topic in data:
-                            for subreddit in data[topic]:
-                                final.append(subreddit)
-                    elif file_name == "topic.json":
-                        for topic in data:
-                            for t in data[topic]:
-                                final.append({"topic": t})
-                    else:
-                        final = [*final, *data]
+    for file in glob.glob(f"{directory}/*"):
+        files.append(file)
+
+for file_name in file_names:
+    final = []
+    for file in files:
+        if file.lower().find(file_name.lower()) != -1:
+            with open(file) as fp:
+                data = json.load(fp)
+                if file_name == "subreddits.json":
+                    for topic in data:
+                        for subreddit in data[topic]:
+                            final.append(subreddit)
+                elif file_name == "topic.json":
+                    for topic in data:
+                        for t in data[topic]:
+                            final.append({"topic": t})
+                else:
+                    final = [*final, *data]
         res = handleOthers(final, file_name)
         with open(f"{file_name}", "w") as fp:
             json.dump(res, fp)
